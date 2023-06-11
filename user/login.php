3725d5168234625e6489101cee513a6f73d7bd39
Desha-Saeed <deshasaeed77@gmail.com>
2023-06-11 17:59:12 +0300

    added validation on inputs for register and login

--- user/login.php
+++ user/login.php
@@ -21,6 +21,33 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["email"]) && isset($_P
     $email = $_POST["email"];
     $password = $_POST["password"];
 
+
+
+
+    // Email validation:
+    if (empty($email)) {
+        $errors[] = "Email is required";
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Invalid email format";
+    }
+
+    // Password validation:
+    if (empty($password)) {
+        $errors[] = "Password is required";
+    } else if (strlen($password) < 8) {
+        $errors[] = "Password must be at least 8 characters";
+    }
+
+    // Check for errors:
+    if (!empty($errors)) {
+        // Display error messages to user:
+        foreach ($errors as $error) {
+            http_response_code(404);
+            echo json_encode(["errors" => $error]);
+            exit();
+        }
+    }
+
     // retrieve user from database
 
     $result = $db->getrow('users', "SELECT * FROM users WHERE email = ?", [$email]);
--- user/register.php
+++ user/register.php
@@ -6,9 +6,44 @@ $db = new Database();
 
 // handle registration
 if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["username"]) && isset($_POST["email"]) && isset($_POST["password"])) {
+
+    //grab data from body
     $username = $_POST["username"];
     $email = $_POST["email"];
-    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+    $password = $_POST["password"];
+
+    // Name validation:
+    if (empty($username)) {
+        $errors[] = "user$username is required";
+    } else if (strlen($username) < 3 || strlen($username) > 50) {
+        $errors[] = "Name must be between 2 and 50 characters";
+    }
+
+    // Email validation:
+    if (empty($email)) {
+        $errors[] = "Email is required";
+    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Invalid email format";
+    }
+
+    // Password validation:
+    if (empty($password)) {
+        $errors[] = "Password is required";
+    } else if (strlen($password) < 8) {
+        $errors[] = "Password must be at least 8 characters";
+    }
+
+    // Check for errors:
+    if (!empty($errors)) {
+        // Display error messages to user:
+        foreach ($errors as $error) {
+            http_response_code(404);
+            echo json_encode(["errors" => $error]);
+            exit();
+        }
+    }
+
+    $passwordHashed = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
 
 
@@ -23,7 +58,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["username"]) && isset(
 
     // insert new user into database
 
-    $new_user = $db->insertRow('users', 'INSERT INTO users (name, email, password) VALUES (? , ? , ?)', [$username, $email, $password]);
+    $new_user = $db->insertRow('users', 'INSERT INTO users (name, email, password) VALUES (? , ? , ?)', [$username, $email, $passwordHashed]);
 
 
     http_response_code(201);
